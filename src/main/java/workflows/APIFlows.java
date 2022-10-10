4f32ebdab27b91c786a8e0aa067df04454f36d39
Olga Kazakov <olga.fitnes.haifa@gmail.com>
2022-10-10 16:48:37 +0300

    update

--- src/main/java/workflows/APIFlows.java
+++ src/main/java/workflows/APIFlows.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import utilities.CommonOps;
 
+
 public class APIFlows extends CommonOps {
 
     @Step ("Business Flow: Get Response From API")
@@ -17,4 +18,31 @@ public class APIFlows extends CommonOps {
         return APIActions.extractFromJson(response, getData("pathHumidity"));
     }
 
+    @Step ("Business Flow: Get Student Data")
+    public static String getStudentData(String jPath) {
+        response = APIActions.get("student/list");
+        return APIActions.extractFromJson(response, jPath);
+    }
+
+    @Step ("Business Flow: Create New Student - API")
+    public static void postStudent(String firstname, String lastname, String email, String programme){
+        params.put("firstName", firstname);
+        params.put("lastName", lastname);
+        params.put("email", email);
+        params.put("programme", programme);
+
+        APIActions.post(params, "/student");
+        System.out.println(response.getBody());
+    }
+
+    @Step ("Business Flow: Create New Student - API")
+    public static void updateStudent(String firstname, String lastname, String email, String programme, String id){
+        params.put("firstName", firstname);
+        params.put("lastName", lastname);
+        params.put("email", email);
+        params.put("programme", programme);
+
+        APIActions.put(params, "/student/" + id);
+        System.out.println(response.getBody());
+    }
 }
